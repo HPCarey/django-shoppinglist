575074dbb2134487ebc444c739d29c36e27a8ccb
HPCarey <hannahpcarey@gmail.com>
2023-05-29 13:21:44 +0000

    create models and migrate

--- list/models.py
+++ list/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class List(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name='items')
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
