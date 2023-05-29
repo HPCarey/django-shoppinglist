478d93147f8c4d4ca67c0c286f621f4591b37e83
HPCarey <hannahpcarey@gmail.com>
2023-05-29 19:04:17 +0000

    add views for index and list, add base templates and wire up urls

--- list/views.py
+++ list/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import List, Item
+
+
+def index(request):
+    lists = List.objects.filter(user=request.user)
+    context = {
+        'lists': lists
+    }
+    return render(request, 'index.html', context)
+
+
+def list(request, id):
+    list = List.objects.get(id=id)
+    context = {
+        'list': list
+    }
+    return render(request, 'list,html', context)
