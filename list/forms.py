af6d1e1880c0f275fe87d8a65030c06449844461
HPCarey <hannahpcarey@gmail.com>
2023-05-30 07:02:57 +0000

    add forms.py and update views and template for creating a shopping list

---
+++ list/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import ModelForm
+from .models import List, Item
+
+
+class ListForm(forms.ModelForm):
+    class Meta:
+        model = List
+        fields = ('name',)
+
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name',)
--- list/views.py
+++ list/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect
 from .models import List, Item
+from .forms import ListForm, ItemForm
+from django.db.models import F
 
 
 def index(request):
     lists = List.objects.filter(user=request.user)
+    form = ListForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            save = form.save(commit=False)
+            save.user = request.user
+            save.save()
     context = {
-        'lists': lists
+        'lists': lists,
+        'form': form
     }
     return render(request, 'index.html', context)
 
 
 def list(request, id):
     list = List.objects.get(id=id)
+    form = ListForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.list = list
+            item.save()
+            return redirect('list', id=id)
     context = {
-        'list': list
+        'list': list,
+        'form': form
     }
     return render(request, 'list.html', context)
